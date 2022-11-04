053eb98a48a9a3d31ab50b304076dad4684cd4cc
Laura C <laurafariacastro@outlook.com>
2022-11-04 00:19:02 -0300

    starting project

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# consumindoAPI
-Consumindo API do Spotify em Python
+# Consumindo API do LinkedIn com Python
+
+Requisições com a biblioteca _requests_  
+Interface utilizando o TKinter
+
+## LinkedIn
+
+[Documentação da API](https://learn.microsoft.com/pt-br/linkedin/consumer/integrations/self-serve/sign-in-with-linkedin)
+
+Necessário ter uma _company page_ para solicitar uso da API.
+
+## Requests
+
+Requisições: Utilizam a biblioteca _requests_  
+[Documentação](https://pypi.org/project/requests/)  
+[Utilização - Python’s Requests Library (Guide)](https://realpython.com/python-requests/#the-response)  
+[Utilização - GET and POST requests using Python](https://www.geeksforgeeks.org/get-post-requests-using-python/)
+
+Trabalhando com o JSON:  
+[Python Create JSON](https://pythonexamples.org/python-create-json/)  
+[Working with JSON in Python](https://datagy.io/python-requests-json/)
+[Parse a JSON response using Python requests library](https://pynative.com/parse-json-response-using-python-requests-library/)
+
+## Interface
+
+TKinter - Já vem instalado no Python
+
+[Documentação](https://docs.python.org/3/library/tkinter.html)  
+[CRIE O PRIMEIRO APLICATIVO GUI USANDO PYTHON-TKINTER](https://acervolima.com/crie-o-primeiro-aplicativo-gui-usando-python-tkinter/)  
+[Create UI in Python-Tkinter](https://www.tutorialsteacher.com/python/create-gui-using-tkinter-python)
+
+## Pacotes
+
+| Package            | Version   |
+| ------------------ | --------- |
+| certifi            | 2022.9.24 |
+| charset-normalizer | 2.1.1     |
+| idna               | 3.4       |
+| pip                | 22.3      |
+| requests           | 2.28.1    |
+| setuptools         | 65.5.0    |
+| urllib3            | 1.26.12   |
+| yapf               | 0.32.0    |
---
+++ server.py
@@ -0,0 +1,89 @@
+import requests
+
+class ServerMethods:
+
+    def __init__(self, URL, token):
+        self.URL = URL
+        self.token = token
+
+
+    def getPersonFullInfo(self):
+        """Obtem dados completos do usuario autenticado com o token
+
+        Returns:
+            json: informacoes sobre o usuario no formato Json
+        """
+        authHeader = {"Authorization": "Bearer "+ self.token}
+        extraInfo = {"projection": "(id,firstName,lastName,profilePicture(displayImage~:playableStreams))"}
+        r = requests.get(url = self.URL + "/me", headers=authHeader, params=extraInfo)
+        return r.json()
+
+
+    def getPersonBasicInfo(self):
+        """Obtem dados basicos do usuario autenticado com o token
+
+        Returns:
+            json: informacoes sobre o usuario no formato Json
+        """
+        authHeader = {"Authorization": "Bearer "+ self.token}
+        r = requests.get(url = self.URL + "/me", headers=authHeader)
+        if r.status_code == 200:
+            return r.json()
+        else:
+            return 'Request Fail' + r.content
+
+
+    def getPersonId(self):
+        """Obtem o identificador da pessoa autenticada com o token
+
+        Returns:
+            string: identificador
+        """
+        resp = self.getPersonBasicInfo()
+        return resp["id"]
+    
+
+    def requestPOST(self, conteudo, descricao, url, tituloUrl):
+        """Realiza uma postagem de conteudo no perfil do usuario autenticado
+
+        Args:
+            conteudo (string): Conteudo principal do post
+            descricao (string): Continuacao do conteudo - breve descricao
+            url (string): URL para redirecionar a alguma outra pagina
+            tituloUrl (string): Titulo da URL adicionada
+
+        Returns:
+            json: Corpo da resposta (contem apenas o ID da publicacao)
+        """
+        authHeader = {"Authorization": "Bearer "+ self.token}
+        personId = self.getPersonId()
+        postBody = {
+                "author": "urn:li:person:"+personId,
+                "lifecycleState": "PUBLISHED",
+                "specificContent": {
+                    "com.linkedin.ugc.ShareContent": {
+                        "shareCommentary": {
+                            "text": conteudo
+                        },
+                        "shareMediaCategory": "ARTICLE",
+                        "media": [
+                            {
+                                "status": "READY",
+                                "description": {
+                                    "text": descricao
+                                },
+                                "originalUrl": url,
+                                "title": {
+                                    "text": tituloUrl
+                                }
+                            }
+                        ]
+                    }
+                },
+                "visibility": {
+                    "com.linkedin.ugc.MemberNetworkVisibility": "CONNECTIONS"
+                }
+        }
+        r = requests.post(url = self.URL + "/ugcPosts", headers=authHeader, data=postBody)
+        return r.json()
+
